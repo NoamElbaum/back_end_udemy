607e8bbd172fa8df1f96fae845bdd32e8130d69d
NoamElbaum <noam272000@gmail.com>
2020-04-28 15:03:28 +0300

    added a second app with html

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from first_app import views
+from first_app.views import index
+from appTwo.views import two
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', index, name='index'),
+    url(r'^two/', two, name='two'),
     url(r'^admin/', admin.site.urls),
 ]
